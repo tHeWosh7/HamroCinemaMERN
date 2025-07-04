623048dc6e5a5067abb579af938e384152c4579d
anynomous <thewosh8@gmail.com>
2025-07-04 16:57:40 +0545

    2_commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import Home from './Pages/Home'
+import Movies from './Pages/Movies'
+import MovieDetails from './Pages/Moviedetails'
+import SeatLayout from './Pages/SeatLayout'
+import MyBookings from './Pages/MyBookings'
+import Favourite from './Pages/Favourite'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+const App = () => {const isAdminRoute = useLocation().pathname.startswith('/admin');
+
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path ="/movies/:id" element={<MovieDetails />} />
+        <Route path ="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path ="/my-bookings" element={<MyBookings />} />
+        <Route path="/favourite" element={<Favourite />} />
+      </Routes>
+    </> 
   )
 }
-
 export default App
---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+       <h1>Navbar</h1>
+       <h2>Welcome to HamroCinema</h2>
+    </div>
+  )
+}
+
+export default Navbar
