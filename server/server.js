b39892caba408334952f59427c6ffeedbcd85a4b
shushovitJR <ranashushovit@gmail.com>
2025-08-24 13:50:18 +0545

    databaseinitialconnection

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,7 @@ await connectDB()
 //Middleware
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 
 //API Routes
