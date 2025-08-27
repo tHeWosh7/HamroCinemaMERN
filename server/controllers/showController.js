73ec5b41893deed6140a434e15b2997f6a8ad0b6
shushovitJR <ranashushovit@gmail.com>
2025-08-27 17:20:45 +0545

    router to get shows from db

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -79,3 +79,39 @@ export const addShow = async (req, res) => {
         res.json({success: false, message: error.message})
     }
 }
+
+//api to get all shows from the database
+export const getShows = async (req, res) => {
+    try{
+        const shows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie').sort({showDateTime: 1});
+
+        //filter unique shows
+        const uniqueShows= new Set(shows.map(show=> show.movie))
+
+        res.json({success: true, shows: Array.from(uniqueShows)})
+    } catch (error){
+        console.error(error);
+        res.json({success: false, message: error.message});
+    }
+}
+
+//api to get a single show from the database
+export const getShow = async (req, res) => {
+    try{
+        const {movieID} = req.params;
+        const shows = await Show.find({movie: movieID, showDateTime: {$gte: new Date()}})
+        const movie = await Movie.findById(movieID);
+        const dateTime = {};
+        shows.forEach((show)=>{
+            const date= show.showDateTime.toISOString().split('T')[0];
+            if(!dateTime[date]){
+                dateTime[date] = [];
+            }
+            dateTime[date].push({time: show.showDateTime, showID: show._id})
+        })
+        res.json({success: true, movie, dateTime})
+    } catch (error){
+        console.error(error);
+        res.json({success: false, message: error.message});
+    }
+}
