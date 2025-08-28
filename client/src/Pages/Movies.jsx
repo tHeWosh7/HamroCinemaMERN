4b73b1c9d2903e13d12b2b4037d13a9dea183cbb
shushovitJR <ranashushovit@gmail.com>
2025-08-28 16:10:59 +0545

    show movies from database

--- client/src/Pages/Movies.jsx
+++ client/src/Pages/Movies.jsx
@@ -1,15 +1,20 @@
-import { dummyShowsData } from "../assets/assets"
+// import { dummyShowsData } from "../assets/assets"
 import BackGradientBlue from "../components/BackGradientBlue"
 import BackGradientRed from "../components/BackGradientRed"
 import MovieCard from "../components/MovieCard"
+import { useAppContext } from "../context/AppContext"
 const Movies = () => {
-  return dummyShowsData.length > 0 ? (
+
+  const {shows} = useAppContext();
+
+
+  return shows.length > 0 ? (
     <div className='relative px-6 md:px-16 lg:px-40 xl:px-44 min-h-[80vh] overflow-hidden'>
           <BackGradientRed top='-150vh' right='50vh'/>
           <BackGradientBlue top='30vh' left='50vh'/>
       <h1 className="text-lg font-medium  mt-35 my-4">Now Showing</h1>
       <div className="flex flex-wrap max-sm:justify-center gap-5 mb-20">
-        {dummyShowsData.map((movie)=>(
+        {shows.map((movie)=>(
           <MovieCard movie={movie} key={movie._id}/>
         ))}
       </div>
