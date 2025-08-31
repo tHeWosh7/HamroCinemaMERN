6493e37c58a13cbf516b09c21cb3eefc464db0b2
anynomous <thewosh8@gmail.com>
2025-08-31 21:13:17 +0545

    esewa

---
+++ client/src/assets/esewa.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import crypto from "crypto";
+
+export async function getEsewaPaymentHash({ amount, transaction_uuid }) {
+  try {
+    const data = `total_amount=${amount},transaction_uuid=${transaction_uuid},product_code=${process.env.ESEWA_PRODUCT_CODE}`;
+    const secretKey = process.env.ESEWA_SECRET_KEY;
+    const hash = crypto.createHmac("sha256", secretKey).update(data).digest("base64");
+
+    return {
+      signature: hash,
+      signed_field_names: "total_amount,transaction_uuid,product_code",
+    };
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function verifyEsewaPayment(encodedData) {
+  try {
+    let decodedData = Buffer.from(encodedData, "base64").toString("utf-8");
+    decodedData = JSON.parse(decodedData);
+
+    const data = `transaction_code=${decodedData.transaction_code},status=${decodedData.status},total_amount=${decodedData.total_amount},transaction_uuid=${decodedData.transaction_uuid},product_code=${process.env.ESEWA_PRODUCT_CODE},signed_field_names=${decodedData.signed_field_names}`;
+    const secretKey = process.env.ESEWA_SECRET_KEY;
+    const hash = crypto.createHmac("sha256", secretKey).update(data).digest("base64");
+
+    if (hash !== decodedData.signature) throw { message: "Invalid Info", decodedData };
+
+    const response = await axios.get(`${process.env.ESEWA_GATEWAY_URL}/api/epay/transaction/status/`, {
+      params: {
+        product_code: process.env.ESEWA_PRODUCT_CODE,
+        total_amount: decodedData.total_amount,
+        transaction_uuid: decodedData.transaction_uuid,
+      },
+      headers: { Accept: "application/json", "Content-Type": "application/json" },
+    });
+
+    if (response.data.status !== "COMPLETE" || response.data.transaction_uuid !== decodedData.transaction_uuid || Number(response.data.total_amount) !== Number(decodedData.total_amount)) {
+      throw { message: "Invalid Info", decodedData };
+    }
+
+    return { response: response.data, decodedData };
+  } catch (error) {
+    throw error;
+  }
+}
